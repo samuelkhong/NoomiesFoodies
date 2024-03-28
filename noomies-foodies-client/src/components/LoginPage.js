a1f39a23a9dcf687191b2b26d086163c764f9cfc
samuel K <samuelkarsengkhong@gmail.com>
2024-03-28 15:45:14 -0700

    Created component folder and LoginPage component and LoginForm.

---
+++ noomies-foodies-client/src/components/LoginPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const LoginPage = () => {
+  return (
+    <>
+        <div class="flex-container">
+            <div class="flex-item">
+                <h1> Noomies Foodies</h1>
+            </div>
+            <div class="flex-container">
+                <h1>Welcome Back!</h1>
+                <p>Login and get started!</p>
+                <LoginForm />
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default LoginPage;
