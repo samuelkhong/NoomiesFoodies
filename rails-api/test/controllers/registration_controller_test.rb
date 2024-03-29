d875b5df8a99d112a1d88763e2d559f03903af08
Creasser <blakecreasser@aggienetwork.com>
2024-03-29 00:52:19 -0500

    add pry gem, working on user tests

--- rails-api/test/controllers/registration_controller_test.rb
+++ rails-api/test/controllers/registration_controller_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
-class RegistrationsControllerTest < ActiveSupport::TestCase 
+class RegistrationsControllerTest < ActionController::TestCase 
+
+  def setup
+    @controller = Users::RegistrationsController.new
+  end
+
     test "should create a new user" do
-    
+  
+      binding.pry
         assert_difference('User.count', 1) do
-          post user_registration_path, params: {email: "testtest@email.com", password: '123456'}, as: :json
+          post '/users/registration', params: {email: "bar@email.com", password: '123456'}, as: :json
         end
         assert_response :created
       end
--- rails-api/test/models/user_test.rb
+++ rails-api/test/models/user_test.rb
@@ -1,15 +1,15 @@
 require "test_helper"
 
-class UserTest < ActiveSupport::TestCase
+class UserTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
 
-  # test "should create a new user" do
+  test "should create a new user" do
     
-  #   assert_difference('User.count', 1) do
-  #     post 'users/registrations', params: {email: "testtest@email.com", password: '123456'}, as: :json
-  #   end
-  #   assert_response :created
-  # end
+    assert_difference('User.count', 1) do
+      post 'users/registrations', params: {email: "foo@email.com", password: '123456'}, as: :json
+    end
+    assert_response :created
+  end
 end
