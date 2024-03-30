6a69c89f467dc3f0f866bc1cc654fd0ed716ff41
samuel K <samuelkarsengkhong@gmail.com>
2024-03-30 16:52:34 -0700

    Added SubmitButton component. In LoginForm added a submitButton instance. Converted Parent return object into a form element. Added dummy event Handler for form submissions.

--- noomies-foodies-client/src/components/LoginForm/LoginForm.js
+++ noomies-foodies-client/src/components/LoginForm/LoginForm.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 import EmailInput from './InputField/EmailInput';
-import PasswordInput from './InputField/PasswordInput'
+import PasswordInput from './InputField/PasswordInput';
+import SubmitButton from './SubmitButton/SubmitButton';
 
 const LoginForm  = () =>  {
     // State variables to store email and password input values 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    // event handler function to handle submit and send information to the backend
+    const handleSubmit = (event) => {
+        event.preventDefault(); // 
+        //toDo
+
+    }
+// returns a form  that has password and email input and submission button. 
 return (
-    <div className='form-group'>
+    <form className='form-group'>
         <label htmlFor="email" className='form-label'>Email address</label>
         <EmailInput value={email} onChange={(event) => setEmail(event.target.value)} />
 
         <label htmlFor="password" className='form-label'>Password</label>
         <PasswordInput value={password} onChange={(event) => setPassword(event.target.value)} />
-    </div>
+
+        <SubmitButton onClick={handleSubmit} />
+    </form>
 )};
 export default LoginForm;
---
+++ noomies-foodies-client/src/components/LoginForm/SubmitButton/SubmitButton.js
@@ -0,0 +1,3 @@
+const SubmitButton = ({ onClick }) => {
+    <button type="submit" onClick={onClick}>Login</button>
+} 
