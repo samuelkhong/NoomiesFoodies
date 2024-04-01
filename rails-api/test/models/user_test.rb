f38f6ae034f085a061b5e34b90ce7a66153f6660
Creasser <blakecreasser@aggienetwork.com>
2024-03-31 19:40:25 -0500

    create tests for account creation, account update, and account deletion

--- rails-api/test/models/user_test.rb
+++ rails-api/test/models/user_test.rb
@@ -1,15 +1,5 @@
 require "test_helper"
 
 class UserTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
-  # test "should create a new user" do
-    
-  #   assert_difference('User.count', 1) do
-  #     post 'users/registrations', params: {email: "foo@email.com", password: '123456'}, as: :json
-  #   end
-  #   assert_response :created
-  # end
 end
