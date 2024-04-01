7492e09cb08f25d1de124fc660ccb7b29f613014
samuel K <samuelkarsengkhong@gmail.com>
2024-04-01 14:55:44 -0700

    Added directory for static images at /public/images and added directory for LoginPage component at /component/LogiPage

--- noomies-foodies-client/src/components/LoginPage.js
+++ noomies-foodies-client/src/components/LoginPage.js
@@ -6,14 +6,14 @@ const LoginPage = () => {
     <>
         <div class="flex-container">
             <div class="flex-item">
-                <img class="logo" src="#"></img>
+                <img class="logo" alt="logo"src="#"></img>
                 <h1> Noomies Foodies</h1>
             </div>
             <div class="flex-item">
                 <h1>Welcome Back!</h1>
                 <p>Login and get started!</p>
                 <LoginForm />
-                <p>Don't have an account yet? <a href='#'>Sign up now</a></p>
+                <p>Don't have an account yet? <a href='#'>Sign up now!</a></p>
             </div>
         </div>
     </>
