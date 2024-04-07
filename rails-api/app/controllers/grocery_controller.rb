f4b0a4a1f274af6f5086833a45aa070e0aae29d9
Creasser <blakecreasser@aggienetwork.com>
2024-04-07 17:31:34 -0500

    create update route for groceries

--- rails-api/app/controllers/grocery_controller.rb
+++ rails-api/app/controllers/grocery_controller.rb
@@ -1,6 +1,18 @@
 class GroceryController < ApplicationController
 
     def update
-        
+        grocery = current_user.find(params[:id])
+        grocery.update(grocery_params)
+        if grocery.valid?
+            render json: grocery, status: :created
+        else
+            render json: { errors: grocery.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
+    end
+
+    protected
+
+    def grocery_params
+        params.permit(:name)
     end
 end
