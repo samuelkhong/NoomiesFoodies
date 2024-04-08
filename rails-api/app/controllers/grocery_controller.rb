d4a67ec282a40f7a78534b9d1578c0c71bee1450
Creasser <blakecreasser@aggienetwork.com>
2024-04-07 20:03:02 -0500

    create update route and delete route

--- rails-api/app/controllers/grocery_controller.rb
+++ rails-api/app/controllers/grocery_controller.rb
@@ -2,11 +2,36 @@ class GroceryController < ApplicationController
 
     def update
         grocery = current_user.find(params[:id])
+        # if grocery[:archived_at] = nil
+        #     grocery.update(grocery_params)
+        # else
+        #     render json: { message: 'Grocery archived'}, status: :unprocessable_entity
+        # end
+
+        # if grocery.valid?
+        #     render json: grocery, status: :created
+        # else
+        #     render json: { errors: grocery.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        # end
         grocery.update(grocery_params)
-        if grocery.valid?
+
+        if grocery[:archived_at] != nil
+            render json: {message: 'Grocery Archived'}
+        elsif grocery.invalid?
+            render json: {errors: grocery.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        elsif 
             render json: grocery, status: :created
+        end
+
+    end
+
+    def destroy
+        grocery = current_user.find(params[:id])
+        if grocery
+            grocery.update(archived_at: Time.current.to_sentence)
+            head :no_content
         else
-            render json: { errors: grocery.errors.full_messages.to_sentence}, status: :unprocessable_entity
+            render json: { message: 'Some error message'}, status: :unprocessable_entity
         end
     end
 
