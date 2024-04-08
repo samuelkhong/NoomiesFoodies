7cf5aae8ff19a93b08877cf2acf0df2b784d6c79
Creasser <blakecreasser@aggienetwork.com>
2024-04-08 15:02:39 -0500

    create migration for creating table for groceries

---
+++ rails-api/db/migrate/20240408194702_create_grocery.rb
@@ -0,0 +1,11 @@
+class CreateGrocery < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groceries do |t|
+      t.string :name
+      t.integer :user_id
+      t.string :archived_at
+
+      t.timestamps
+    end
+  end
+end
