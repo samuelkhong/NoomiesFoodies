23a8216e544634ed0682453a503ddcf4e1094f1d
samuel K <samuelkarsengkhong@gmail.com>
2024-04-09 16:45:35 -0700

    Added Setup Files for dashboard component.

---
+++ noomies-foodies-client/src/components/DashBoard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import SideMenu from '../SideMenu/SideMenu';
+import TopMenu from '../TopMenu/TopMenu';
+
+const Dashboard = () => {
+
+    return (
+
+
+    );
+};
+export default Dashboard;
