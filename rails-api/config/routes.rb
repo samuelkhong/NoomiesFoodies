84727610027816587e24ca007c9a8af674954317
Creasser <blakecreasser@aggienetwork.com>
2024-04-08 23:39:30 -0500

    create CRUD routes and open them in routes.rb

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  resources :fridge, only: [:create, :update, :show, :index, :destroy]
+
   # devise_scope :user do
   #   get "sign_up", to: 'users/registrations#create'
   # end
