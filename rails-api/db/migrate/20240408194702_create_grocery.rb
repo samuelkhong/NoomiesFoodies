35a54d4dae75bf0ac8a2fe731f5372a547061ffc
Creasser <blakecreasser@aggienetwork.com>
2024-04-08 21:45:16 -0500

    create grocery table migration

--- rails-api/db/migrate/20240408194702_create_grocery.rb
+++ rails-api/db/migrate/20240408194702_create_grocery.rb
@@ -1,11 +1,13 @@
 class CreateGrocery < ActiveRecord::Migration[7.0]
   def change
     create_table :groceries do |t|
-      t.string :name
-      t.integer :user_id
       t.string :archived_at
       t.boolean :universal
-
+      t.string :brand, limit: 100
+      t.string :quantity, limit: 100
+      t.references :user, foreign_key: true
+      #t.references :food, foreign_key: true
+      
       t.timestamps
     end
   end
--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_29_022217) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_08_194702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groceries", force: :cascade do |t|
+    t.string "archived_at"
+    t.boolean "universal"
+    t.string "brand", limit: 100
+    t.string "quantity", limit: 100
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groceries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_29_022217) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groceries", "users"
 end
