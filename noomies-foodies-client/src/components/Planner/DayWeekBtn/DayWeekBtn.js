a2dbf2df8209a2f4a78063b19e0857540a26b1bd
samuel K <samuelkarsengkhong@gmail.com>
2024-04-10 16:14:55 -0700

    Added jsx structure for day and week btns. Added functions that detect toggle day and week state.

--- noomies-foodies-client/src/components/Planner/DayWeekBtn/DayWeekBtn.js
+++ noomies-foodies-client/src/components/Planner/DayWeekBtn/DayWeekBtn.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const DayWeekBtn = () => {
 
+    // intialize the current state holding variables
+    const [isDay, setIsDay] = useState(true); 
+    const [isWeek, setIsWeek] = useState(false);
+
+    // sets the day state
+    const setDayView = () => {
+        setIsDay(True);
+        setIsWeek(False);
+    }
+
+    //set the view to week
+    const setWeekView = () => {
+        setIsDay(False);
+        setIsWeek(True);
+    }
+
+  // returns two buttons. day btn when clicked sets  isDay to true and is week to false. 
+  // sets today btn to active state. 
     return (
-        <>
-        </>
+        <div className="day-week-toggle">
+            <button className={`day ${isDay ? 'active' : ''}`} onClick={setDayView}></button>
+            <button className={`day ${isWeek ? 'active' : ''}`} onClick={setWeekView}></button>
+        </div>
     );
 }
 export default DayWeekBtn;
