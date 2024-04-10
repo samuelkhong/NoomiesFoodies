a88627585b5989e38ae91076f6d391a606d66a65
Creasser <blakecreasser@aggienetwork.com>
2024-04-10 16:13:27 -0500

    create update route for fridge items

--- rails-api/app/controllers/fridge_item_controller.rb
+++ rails-api/app/controllers/fridge_item_controller.rb
@@ -1,7 +1,7 @@
 class FridgeItemController < ApplicationController
 
     def create
-        fridge = find_fridge(params[:id])
+        fridge = find_fridge(params[:fridge_id])
         new_fridge_item = fridge.fridge_items.create(fridge_item_params)
 
         if new_fridge_item.valid?
@@ -12,14 +12,30 @@ class FridgeItemController < ApplicationController
         
     end
 
+    def update
+        fridge_item = find_fridge_item(params[:fridge_item_id])
+
+        if fridge_item.update(fridge_item_params)
+            render json: fridge_item, status: :ok
+        else
+            render json: { message: fridge_item.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
+    end
+
     protected
 
     def fridge_item_params
-        params.permit(:grocery_id)
+        params.permit(:grocery_id, :quantity)
     end
 
-    def find_fridge
-        current_user.fridges.find_by(id: params[:id])
+    def find_fridge(fridge_id)
+        fridge = current_user.fridges.find_by(id: fridge_id)
+    end
+
+    def find_fridge_item(fridge_item_id)
+        fridge = find_fridge(params[:fridge_id])
+        fridge_item = fridge.fridge_items.find_by(id: fridge_item_id)
+        return fridge_item
     end
 
 end
