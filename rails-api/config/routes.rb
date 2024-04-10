4ff443242180c80fd457db6c34f3b175656536fd
Creasser <blakecreasser@aggienetwork.com>
2024-04-10 16:48:50 -0500

    open routes in routes.rb

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :grocery, only: [:index]
+  resources :fridge_item, only: [:create, :update, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
