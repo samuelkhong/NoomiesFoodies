6e5c679dbf15fa402f21cc6b6056829bfc431b72
samuel K <samuelkarsengkhong@gmail.com>
2024-04-09 17:40:10 -0700

    Added switch element placeholder. Will selectively load components to the dashboard based on the route.

--- noomies-foodies-client/src/components/DashBoard/Dashboard.js
+++ noomies-foodies-client/src/components/DashBoard/Dashboard.js
@@ -1,12 +1,29 @@
 import React, {useState} from 'react';
 import SideMenu from '../SideMenu/SideMenu';
 import TopMenu from '../TopMenu/TopMenu';
+import './Dashboard.css'
+//import more components here 
+
 
 const Dashboard = () => {
 
     return (
+        <div className='flex-container'>
+            <div className='flex-item-left'>
+                <SideMenu username="sam" userProfile=""></SideMenu>
+
+            </div>
+            <div className='flex-item-right'>   
+                <TopMenu alertNum={3} emailNum={5}></TopMenu>
+                {/* return the appropriate component based on the url*/ }
+                <switch>
+                    {/* add more routes and components here*/ }
+                    <Route path="" component={""}></Route>
+                </switch>
 
 
+            </div>
+        </div>
     );
 };
 export default Dashboard;
