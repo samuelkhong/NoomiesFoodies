3bd478e269cfbe17e49d56b3e5aa6a5a04f6bae2
samuel K <samuelkarsengkhong@gmail.com>
2024-04-11 16:05:08 -0700

    Added starter skeleton code for CalorieCounter component.

---
+++ noomies-foodies-client/src/components/Planner/CalorieCounter/CalorieCounter.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import './CalorieCounter.css'
+
+const CalorieCounter =  () => {
+    return (
+        <>
+        </>
+    );
+};
+export default  CalorieCounter;
