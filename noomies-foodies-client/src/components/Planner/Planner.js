36771fb37c2038307ce2eca4aab4b5101fa02e75
samuel K <samuelkarsengkhong@gmail.com>
2024-04-12 14:03:26 -0700

    Planner will now hold isDay state react hook. Planner will pass isDay and set isDay as props to DayWeekBtn. Planner now renders today properly with correct styling. Can switch rendered page when isDay is changed. Stylized. Not connected to backend endpoints.

--- noomies-foodies-client/src/components/Planner/Planner.js
+++ noomies-foodies-client/src/components/Planner/Planner.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DayWeekBtn from "./DayWeekBtn/DayWeekBtn";
 import MealsTodayAll from "./MealsTodayAll/MealsTodayAll";
-
+import DayMealCard from './DayMealCard/DayMealCard';
+import './Planner.css'
 
 const Planner = () => {
+    const [isDay, setIsDay] = useState(true); 
+    // listens to isDay. If changes, changes the component
+    useEffect(() => {
+        //todo send a fetch request from the api getting all meals for today or week
+        // feed that information back to be rendered
+
+
+    }, [isDay]) //dependency array. Use Effect is called whenever isDay changes
+
+
+
     return (
-        <>
-        </>
+        <div className='Planner-container'>
+            <DayWeekBtn isDay={isDay} setIsDay={setIsDay}></DayWeekBtn>
+            {/* renders componenet based on boolean isDay */}
+            {/* temp placeholder daymealcard. Will eventually render MealsWeekAll */}
+            {isDay ? <MealsTodayAll></MealsTodayAll> : <DayMealCard></DayMealCard>}
+        </div>
     );
 };
 export default Planner;
