dbb29db4f5fb529fb71629ee8edfd0a61c970ea3
samuel K <samuelkarsengkhong@gmail.com>
2024-04-12 16:28:05 -0700

    Created component for small meal cards for week and stylized.

---
+++ noomies-foodies-client/src/components/Planner/MealCardSmall/MealCardSmall.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import CalorieCounter from '../CalorieCounter/CalorieCounter';
+import './MealCardSmall.css'
+
+const MealCardSmall = () => {
+    return(
+        <div className='meal-small-card'>
+            <h3 className='meal-time-week'>Breakfast</h3>
+            <img className="recipe-small-card" src="./images/planner-icons/sample-recipe.png" alt="meal"></img>
+            <h3 className='small-card-meal-title'>High-Protein Scrambled Eggs with Cottage Cheese</h3>
+            <div className='calorie-wrapper'>
+                <CalorieCounter></CalorieCounter>
+            </div>
+            <button className='options-btn' ><img src="./images/planner-icons/options-small-card.png" alt="options"></img></button>
+
+        </div>
+
+    ); 
+};
+export default MealCardSmall;
