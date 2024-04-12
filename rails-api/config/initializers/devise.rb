166e6e75ad2cbd59211ab4f28ab9a377ab977f6c
Donte Ladatto <donte279@gmail.com>
2024-04-11 19:43:57 -0500

    correct action_mailer config

--- rails-api/config/initializers/devise.rb
+++ rails-api/config/initializers/devise.rb
@@ -311,8 +311,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 8080 }
-
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
     jwt.dispatch_requests = [
