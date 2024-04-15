f8c68b3df5b8110e38e85b838d4c4a82fcf3f87c
Creasser <blakecreasser@aggienetwork.com>
2024-04-15 18:48:42 -0500

    open routes in route.rb file

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -6,19 +6,20 @@ Rails.application.routes.draw do
   }
 
   resources :fridge, only: [:create, :update, :show, :index]
+  resources :grocery, only: [:index]
+  resources :fridge_item, only: [:create, :update]
+  resources :shopping_list, only: [:index, :show, :create, :update, :destroy]
+  resources :recipe, only: [:index, :show, :create, :update, :destroy]
 
   # devise_scope :user do
   #   get "sign_up", to: 'users/registrations#create'
   # end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :grocery, only: [:index]
-  resources :fridge_item, only: [:create, :update]
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :shopping_list, only: [:index, :show, :create, :update, :destroy]
 
 
 end
