60c5ac3fbe41e46c4a2bdc0772e12e44287f0b62
samuel K <samuelkarsengkhong@gmail.com>
2024-04-15 14:11:51 -0700

    Created component that loads all meals for a single day with styling. Routes not connected. Hard coded and not responsive.

---
+++ noomies-foodies-client/src/components/Planner/WeekMealCard/DayRowMeals/DayRowMeals.js
@@ -0,0 +1,29 @@
+import React from "react";
+import MealCardSmall from "../../MealCardSmall/MealCardSmall";
+import CalorieCounter from "../../CalorieCounter/CalorieCounter";
+import TotalCookingTime from "../../TotalCookingTime/TotalCookingTime"
+import './DayRowMeals.css';
+
+const DayRowMeals = ({weekday, date, arrMeals}) => {
+    return(
+        <div className="row-meal-info-container">
+            <div className="day-meal-info">
+                <div className="date">{weekday}:{date}</div>
+                <div className="calorie-container"><CalorieCounter></CalorieCounter></div>
+                <div className="time-container"><TotalCookingTime></TotalCookingTime></div>
+            </div>
+
+            <div className="meal-container">
+                {arrMeals.map((meal) => (
+                    <MealCardSmall></MealCardSmall>
+                 ))}
+
+
+            </div>
+
+        </div>
+        
+        
+    );
+};
+export default DayRowMeals;
