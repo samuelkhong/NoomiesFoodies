235382388b090d923499e69666c6239976f18436
samuel K <samuelkarsengkhong@gmail.com>
2024-04-16 14:06:08 -0700

    MealsTodayAll now takes dynamic data and calculates total calories and time.

--- noomies-foodies-client/src/components/Planner/MealsTodayAll/MealsTodayAll.js
+++ noomies-foodies-client/src/components/Planner/MealsTodayAll/MealsTodayAll.js
@@ -4,32 +4,51 @@ import TotalCookingTime from '../TotalCookingTime/TotalCookingTime';
 import DayMealCard from '../DayMealCard/DayMealCard';
 import './MealsTodayAll.css'
 
-const MealsTodayAll = () => {
+const MealsTodayAll = ({ arrMeals }) => {
+    let totalTime = 0;
+    // sum up calories
+    const totalCalories = arrMeals.reduce((total, current) => {
+        totalTime = current.cookTime + totalTime;
+        return total + current.calories; // Accumulate the calories of each meal
+    }, 0);
+    
+    totalTime = totalTime / 60;
+    // round up or down the amount of time to 1 decimal place
+    let roundedTime = Math.floor(totalTime);
+    if ((totalTime - roundedTime) > .05) {
+        roundedTime += .1;
+    }
 
-
-    const dummyMeal = "High Protein Scrambled Eggs with Cottage Cheese";
-    const dummyMealTime = "breakfast";
-    const dummyPrep = 10;
-    const dummyCook = 10;
-    const dummyCal = 200;
-
+    
     return (
         <div className='meals-today-all'>
             <div className='day-meal-summary'>
                 <h1>Meals</h1>
                 <div className='calorie-summary'>
-                    <CalorieCounter></CalorieCounter>
+                    <CalorieCounter calorieCount={totalCalories}></CalorieCounter>
                 </div>
                 <div className='time-summary'>
-                    <TotalCookingTime></TotalCookingTime>
+                    <TotalCookingTime time={roundedTime}></TotalCookingTime>
                 </div>
             </div>
             <div className='day-meal-container'>
-                <div className='day-meal-item'><DayMealCard></DayMealCard></div>
-                <div className='day-meal-item'><DayMealCard></DayMealCard></div>
-                <div className='day-meal-item'><DayMealCard></DayMealCard></div>
+                {arrMeals.map((meal, index) => {
+                    return (
+                        <div className='day-meal-item' key={index}>
+                            <DayMealCard
+                                mealTitle={meal.mealTitle}
+                                mealTime={meal.mealTime}
+                                calories={meal.calories}
+                                cookTime={meal.cookTime}
+                                imgUrl={meal.imgUrl}
+                                prepTime={meal.prepTime}
+                            />
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
 };
+
 export default MealsTodayAll;
