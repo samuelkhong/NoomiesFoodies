60bcb7792d5e484ae3b4f48019db9d8afe486985
Creasser <blakecreasser@aggienetwork.com>
2024-04-15 22:17:47 -0500

    create update route for foods

--- rails-api/app/controllers/food_controller.rb
+++ rails-api/app/controllers/food_controller.rb
@@ -10,10 +10,32 @@ class FoodController < ApplicationController
         end
     end
 
+    def update
+        food = find_food(params[:id])
+
+        if food && food.update(food_params)
+            render json: food, status: :created
+        elsif food = nil
+            render json: { message: 'Food not found'}, status: :unprocessable_entity
+        else
+            render json: { errors: food.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
+    end
+
     protected
 
     def food_params
         params.permit(:name, :description)
+    end
+
+    def find_food(id)
+        food = current_user.foods.find_by(id: id)
+
+        if food && food.archived_at = nil
+            return food
+        else
+            return nil
+        end
     end
 
 end
