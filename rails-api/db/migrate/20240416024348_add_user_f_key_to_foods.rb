c6a8ab1e815472aa645fca7729128809bdd1a48d
Creasser <blakecreasser@aggienetwork.com>
2024-04-15 21:47:55 -0500

    change add_column to add_reference

--- rails-api/db/migrate/20240416024348_add_user_f_key_to_foods.rb
+++ rails-api/db/migrate/20240416024348_add_user_f_key_to_foods.rb
@@ -1,5 +1,5 @@
 class AddUserFKeyToFoods < ActiveRecord::Migration[7.0]
   def change
-    add_column :foods, :user, foreign_key: true
+    add_reference :foods, :user, foreign_key: true
   end
 end
