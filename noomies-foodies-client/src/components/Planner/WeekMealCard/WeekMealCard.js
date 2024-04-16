c499487b53a63af2d57042c94e34fd5b0eddce0a
samuel K <samuelkarsengkhong@gmail.com>
2024-04-15 18:52:39 -0700

    Converted to dynamic data in props used by WeekMealCard.js

--- noomies-foodies-client/src/components/Planner/WeekMealCard/WeekMealCard.js
+++ noomies-foodies-client/src/components/Planner/WeekMealCard/WeekMealCard.js
@@ -2,14 +2,54 @@ import React from "react";
 import DayRowMeals from "./DayRowMeals/DayRowMeals";
 import './DayRowMeals.css';
 
-const WeekMealCard = () => {
+const WeekMealCard = ({arrMealsWeek}) => {
 
-    const arrMealsWeek =[1, 2, 3, 4, 5, 6, 7];
+    // arrNealsWeek will be an array of objects
+    // each object will represent a day 
+    // each object will have a weekday, date and array of meals
+
+    arrMealsWeek = [
+        {
+            day: 'Monday',
+            date: '2024-04-01',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        },
+        {
+            day: 'Tuesday',
+            date: '2024-04-02',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        },
+        {
+            day: 'Wednesday',
+            date: '2024-04-03',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        },
+        {
+            day: 'Thursday',
+            date: '2024-04-04',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        },
+        {
+            day: 'Friday',
+            date: '2024-04-05',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        },
+        {
+            day: 'Saturday',
+            date: '2024-04-06',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        },
+        {
+            day: 'Sunday',
+            date: '2024-04-07',
+            meals: ['Breakfast', 'Lunch', 'Dinner']
+        }
+    ];
     return (
     <div className="week-meals-container">
         <h1 className="week-container-heading">Meals</h1>
         { arrMealsWeek.map((meal) => (
-            <DayRowMeals weekday={'Monday'} date={'4/7/23'} arrMeals={[1, 2, 3]}></DayRowMeals>
+            <DayRowMeals weekday={meal.day} date={meal.date} arrMeals={meal.meals}></DayRowMeals>
         ))};
     </div>
     );
