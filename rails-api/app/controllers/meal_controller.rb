d4892b5d6c4ccc06d12ccfacf54fbe2675df9da8
Donte Ladatto <donte279@gmail.com>
2024-04-17 16:22:34 -0500

    get all meals

--- rails-api/app/controllers/meal_controller.rb
+++ rails-api/app/controllers/meal_controller.rb
@@ -1,2 +1,12 @@
 class MealController < ApplicationController
+
+  def index
+    meals = current_user.meals.where(archived_at: nil).includes(:foods)
+    if meals.empty?
+        render json: { message: 'You have no meals'}, status: :ok
+    else
+        render json: meals, include: {foods: { only: [:id, :name, :description]}}, status: :ok
+    end
+  end
+  
 end
