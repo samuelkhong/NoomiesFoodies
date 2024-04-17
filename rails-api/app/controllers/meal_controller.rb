e9c3694203971de062f6b9fe63646106a0bf427b
Donte Ladatto <donte279@gmail.com>
2024-04-17 16:28:53 -0500

    get one meal + strong params & find method

--- rails-api/app/controllers/meal_controller.rb
+++ rails-api/app/controllers/meal_controller.rb
@@ -3,10 +3,29 @@ class MealController < ApplicationController
   def index
     meals = current_user.meals.where(archived_at: nil).includes(:foods)
     if meals.empty?
-        render json: { message: 'You have no meals'}, status: :ok
+        render json: { message: "You have no meals yet!" }, status: :ok
     else
         render json: meals, include: {foods: { only: [:id, :name, :description]}}, status: :ok
     end
+  end
+
+  def show
+    meal = find_meal(params[:id])
+    if meal.archived_at.present?
+        render json: { message: "Whoops, that meal doesn't exist!" }, status: :unprocessable_entity
+    else
+        render json: meal, status: :ok
+    end
+  end
+
+private
+
+  def meal_params
+    params.permit(:category, :description)
+  end
+
+  def find_meal(id)
+    current_user.meals.find_by(id: id)
   end
   
 end
