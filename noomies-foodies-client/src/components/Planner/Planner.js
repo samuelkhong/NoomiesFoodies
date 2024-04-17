c7842685dbea99f0a4dc5e78dbef3f257f41101f
samuel K <samuelkarsengkhong@gmail.com>
2024-04-16 18:48:46 -0700

    removed the need to send props to the planner dashboard. TODO added for getting an async function to get revelant meal data. Temp added dummy data for visualization.

--- noomies-foodies-client/src/components/Planner/Planner.js
+++ noomies-foodies-client/src/components/Planner/Planner.js
@@ -5,7 +5,96 @@ import DayMealCard from './DayMealCard/DayMealCard';
 import WeekMealCard from './WeekMealCard/WeekMealCard';
 import './Planner.css'
 
-const Planner = ({dayArr, weekArr}) => {
+const Planner = () => {
+    //todo add async functions to have get request for data. 
+    const arrMealsWeek = [
+        {
+            weekday: "Monday",
+            date: "4/1/2024",
+            meals: [
+                {
+                    mealTitle: "High-Protein Scrambled Eggs with Cottage Cheese",
+                    mealTime: "Breakfast",
+                    calories: 350,
+                    time: 40,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    mealTitle: "Grilled Chicken Salad with Avocado",
+                    mealTime: "Lunch",
+                    calories: 500,
+                    time: 30,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    mealTitle: "Spaghetti Squash with Turkey Meatballs",
+                    mealTime: "Dinner",
+                    calories: 450,
+                    time: 10,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                }
+            ]
+        },
+        {
+            weekday: "Tuesday",
+            date: "4/2/2024",
+            meals: [
+                {
+                    mealTitle: "Oatmeal with Berries",
+                    mealTime: "Breakfast",
+                    calories: 300,
+                    time: 20,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    mealTitle: "Grilled Salmon with Quinoa",
+                    mealTime: "Lunch",
+                    calories: 550,
+                    time: 25,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    mealTitle: "Vegetable Stir-Fry with Tofu",
+                    mealTime: "Dinner",
+                    calories: 400,
+                    time: 200,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        }
+        
+       
+    ];
+
+    const meals =  [
+        {
+            mealTitle: "Oatmeal with Berries",
+            mealTime: "Breakfast",
+            calories: 300,
+            time: 20,
+            imgUrl: "./images/planner-icons/sample-recipe.png"
+        },
+        {
+            mealTitle: "Grilled Salmon with Quinoa",
+            mealTime: "Lunch",
+            calories: 550,
+            time: 25,
+            imgUrl: "./images/planner-icons/sample-recipe.png"
+        },
+        {
+            mealTitle: "Vegetable Stir-Fry with Tofu",
+            mealTime: "Dinner",
+            calories: 400,
+            time: 200,
+            imgUrl: "./images/planner-icons/sample-recipe.png"   
+        }
+    ]
+
+
+
+
+
+
     const [isDay, setIsDay] = useState(true); 
     // listens to isDay. If changes, changes the component
     useEffect(() => {
