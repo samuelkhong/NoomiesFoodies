580e5b8836e87f995c2776cbbea257cfe01f2a5f
samuel K <samuelkarsengkhong@gmail.com>
2024-04-17 23:28:48 -0700

    Added Recipes page that shows all meal times. Currently cannot switch to view all recipes. Add recipes btn does not change page.

---
+++ noomies-foodies-client/src/components/Recipes/Recipes.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './Recipes.css'
+import RecipeBtn from "./RecipeBtn/RecipeBtn"
+import RecipeRow from './RecipeRow/RecipeRow';
+import EmptyCard from './EmptyCard/EmptyCard';
+
+
+const Recipe = ({breakfast, lunch, dinner, other}) => { 
+    lunch = false;   
+    return(
+        <div className='recipe-container'>
+            <div className='recipe-header'>
+                <div className='top-add-btn'>
+                    <RecipeBtn imgUrl={'./images/recipes-icon/plus.png'} content={'Add Recipe'} ></RecipeBtn>
+                </div>
+                
+                <h1 className='recipe-title'>My Recipes</h1>
+            </div>
+            <div className='meal-time' >
+                <div className='recipe-category-title'>
+                    <p>Breakfast:</p>
+                    <btn className="breakfast-btn view-all">View All</btn>
+                </div>
+                {breakfast ? (
+                    <RecipeRow arrMeals={breakfast} />
+                ) : (
+                    <div className="no-recipe-container">
+                        <div className='empty-card-container'><EmptyCard/></div>
+                        
+                        <RecipeBtn imgUrl={'./images/recipes-icon/plus.png'} content={'Add Recipe'} />
+                    </div>
+                )}
+
+                {/* <RecipeRow arrMeals={breakfast}></RecipeRow> */}
+            </div >
+            <div className='meal-time' >
+                <div className='recipe-category-title'>
+                    <p>Lunch:</p>
+                    <btn className="lunch-btn view-all">View All</btn>
+                </div>
+                {lunch ? (
+                    <RecipeRow arrMeals={lunch} />
+                ) : (
+                    <div className="no-recipe-container">
+                        <div className='empty-card-container'><EmptyCard/></div>
+                        
+                        <RecipeBtn imgUrl={'./images/recipes-icon/plus.png'} content={'Add Recipe'} />
+                    </div>
+                )}
+            </div >
+            <div className='meal-time' >
+                <div className='recipe-category-title'>
+                    <p>Dinner:</p>
+                    <btn className="dinner-btn view-all">View All</btn>
+                </div>
+                {dinner ? (
+                    <RecipeRow arrMeals={dinner} />
+                ) : (
+                    <div className="no-recipe-container">
+                        <div className='empty-card-container'><EmptyCard/></div>
+                        
+                        <RecipeBtn imgUrl={'./images/recipes-icon/plus.png'} content={'Add Recipe'} />
+                    </div>
+                )}
+            </div >
+            <div className='meal-time' >
+                <div className='recipe-category-title'>
+                    <p>Snacks/Other:</p>
+                    <btn className='other-btn view-all'>View All </btn>
+                </div>
+                {other ? (
+                    <RecipeRow arrMeals={other} />
+                ) : (
+                    <div className="no-recipe-container">
+                        <div className='empty-card-container'><EmptyCard/></div>
+                        
+                        <RecipeBtn imgUrl={'./images/recipes-icon/plus.png'} content={'Add Recipe'} />
+                    </div>
+                )}
+            </div >
+                
+        </div>
+
+    ); 
+};
+export default Recipe;
