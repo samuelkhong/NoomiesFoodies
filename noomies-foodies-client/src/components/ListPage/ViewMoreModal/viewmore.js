8720e8aab8985d8ae47fc50adc7c77dd746d7962
Usman Raja <usmanraja90@gmail.com>
2024-04-18 21:17:18 -0500

    fixed view more modal css styling

--- noomies-foodies-client/src/components/ListPage/ViewMoreModal/viewmore.js
+++ noomies-foodies-client/src/components/ListPage/ViewMoreModal/viewmore.js
@@ -1,6 +1,6 @@
 import './viewmore.css'
 
-function ViewMoreModal ({onDeleteClick, closeModal}) {
+function ViewMoreModal ({onDeleteClick, onEditClick, closeModal}) {
     return (
           
         <div className="modal-container">
@@ -8,9 +8,9 @@ function ViewMoreModal ({onDeleteClick, closeModal}) {
                 <img src="./images/list-images/view-close.png"></img>
             </button>
             <div className="modal-content">
-                <div className="modal-text">Edit this list</div>
+                <button  className="modal-text" onClick={onEditClick} >Edit this list</button>
                 <div><hr className="horizontal-line"/></div>
-                <button onClick={onDeleteClick} className="delete-button">Delete</button>
+                <button  className="delete-button" onClick={onDeleteClick} >Delete</button>
             </div> 
         </div>
         
