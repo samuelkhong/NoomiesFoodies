b00dea692153d774c125f1b08b12bcd3ee083eed
samuel K <samuelkarsengkhong@gmail.com>
2024-04-20 12:33:28 -0700

    Added back btn for changing active state within the page.

--- noomies-foodies-client/src/components/Recipes/BackBtn/BackBtn.js
+++ noomies-foodies-client/src/components/Recipes/BackBtn/BackBtn.js
@@ -2,8 +2,13 @@ import React from "react";
 import './BackBtn.css'
 // prop is onclick function from parent element
 const BackBtn = ({ onBackButtonClick }) => {
+    const handleClick = () => {
+        if (onBackButtonClick) {
+            onBackButtonClick(); // Call the onClick function if it exists
+        }
+    };
     return (
-        <button className="back-btn">
+        <button className="back-btn" onClick={handleClick}>
             <img className="back-img" src="./images/recipes-icon/back-button.png" alt="back"></img>
         </button>
     )
