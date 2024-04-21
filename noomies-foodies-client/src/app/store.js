95d219e03e3d6c89dc2eac42609973f88e4509d7
Donte Ladatto <donte279@gmail.com>
2024-04-21 18:09:16 -0500

    add api and auth reducers

--- noomies-foodies-client/src/app/store.js
+++ noomies-foodies-client/src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from '../features/apiSlice'
+import authReducer from '../features/authSlice'
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true
 })
