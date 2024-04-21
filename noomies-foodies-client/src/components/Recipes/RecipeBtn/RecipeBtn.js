262b76a59813d907118dc1d0286d9fa12c91931d
samuel K <samuelkarsengkhong@gmail.com>
2024-04-21 12:35:46 -0700

    changed recipeBtn to be a btn. Accepts onClick function as a prop.

--- noomies-foodies-client/src/components/Recipes/RecipeBtn/RecipeBtn.js
+++ noomies-foodies-client/src/components/Recipes/RecipeBtn/RecipeBtn.js
@@ -1,12 +1,12 @@
 import React from 'react'; // Import React
 import "./RecipeBtn.css"
 
-const RecipeBtn = ({ btnUrl, imgUrl, content}) => {
+const RecipeBtn = ({ onClick, imgUrl, content}) => {
     return (
-        <a className="btn-container" href={btnUrl} >
+        <button className="btn-container" href={btnUrl} >
             <img className="btn-pic" src={imgUrl}></img>
             <p className='btn-text'>{content}</p>
-        </a>
+        </button>
     );
 
 };
