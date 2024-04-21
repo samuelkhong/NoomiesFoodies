ddd256f13582f0fd3b84e65a3e0d1150f7f0b15d
Donte Ladatto <donte279@gmail.com>
2024-04-21 18:22:37 -0500

    add user associations and serialize them

--- rails-api/app/models/user.rb
+++ rails-api/app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
-  has_many :ShoppingLists
+  has_one :fridge
+  has_many :shopping_lists
+  has_many :meals
+  has_many :recipes
+  has_many :groceries
+  has_many :foods
 end
