2fd3abb3520fca77af9ad2e6d4e95ab4a16c2df8
samuel K <samuelkarsengkhong@gmail.com>
2024-04-21 12:36:25 -0700

    fixed onClick btn not connecting to onclick property.

--- noomies-foodies-client/src/components/Recipes/RecipeBtn/RecipeBtn.js
+++ noomies-foodies-client/src/components/Recipes/RecipeBtn/RecipeBtn.js
@@ -3,7 +3,7 @@ import "./RecipeBtn.css"
 
 const RecipeBtn = ({ onClick, imgUrl, content}) => {
     return (
-        <button className="btn-container" href={btnUrl} >
+        <button className="btn-container" onClick={onClick} >
             <img className="btn-pic" src={imgUrl}></img>
             <p className='btn-text'>{content}</p>
         </button>
