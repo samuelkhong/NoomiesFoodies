a26c8cf1c65f50d053d80e7298f18091bbee3c95
Donte Ladatto <donte279@gmail.com>
2024-04-22 22:45:56 -0500

    add login/dashboard render logic

--- noomies-foodies-client/src/App.js
+++ noomies-foodies-client/src/App.js
@@ -1,19 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import TopMenu from './components/TopMenu/TopMenu';
-import SideMenu from  './components/SideMenu/SideMenu';
+import SideMenu from './components/SideMenu/SideMenu';
 import Button from './components/ListPage/buttons/button';
 import SideBtn from './components/SideMenu/SideButton/SideButton';
 import ListPage from './components/ListPage/ListPage';
+import LoginPage from './components/LoginPage/LoginPage'
+import Dashboard from './components/DashBoard/Dashboard.js'
+import { useSelector } from 'react-redux';
+
 
 function App() {
-  
+
+  const user = useSelector(state => state.auth.user)
 
   return (
-    <div className="App"> 
-      
-      <ListPage />
-      
+    <div className="App">
+
+      {user ? <Dashboard /> : <LoginPage />}
+
     </div>
   );
 }
