f3ad1e7b42cd5072c4cc64d8a3620ba7ea53340b
samuel K <samuelkarsengkhong@gmail.com>
2024-04-23 03:57:05 -0700

    styling error changes.

--- noomies-foodies-client/src/components/Planner/MealsTodayAll/MealsTodayAll.js
+++ noomies-foodies-client/src/components/Planner/MealsTodayAll/MealsTodayAll.js
@@ -5,13 +5,21 @@ import DayMealCard from '../DayMealCard/DayMealCard';
 import './MealsTodayAll.css'
 
 const MealsTodayAll = ({ arrMeals }) => {
-    let totalTime = 0;
-    // sum up calories
-    const totalCalories = arrMeals.reduce((total, current) => {
-        totalTime = current.cookTime + totalTime;
-        return total + current.calories; // Accumulate the calories of each meal
-    }, 0);
     
+    let totalTime = 0;
+    let totalPrep = 0;
+    let totalCalories = 0;
+    // sum up calories
+    if (arrMeals) {
+        totalCalories = arrMeals.reduce((total, current) => {
+            totalTime = current.time + totalTime;
+            totalPrep = current.prepTime + totalPrep; // total prep time
+            return total + current.calories; // Accumulate the calories of each meal
+        }, 0);
+    }
+
+    // sum total cooking and prep time
+    totalTime += totalPrep;
     totalTime = totalTime / 60;
     // round up or down the amount of time to 1 decimal place
     let roundedTime = Math.floor(totalTime);
@@ -39,7 +47,7 @@ const MealsTodayAll = ({ arrMeals }) => {
                                 mealTitle={meal.mealTitle}
                                 mealTime={meal.mealTime}
                                 calories={meal.calories}
-                                cookTime={meal.cookTime}
+                                cookTime={meal.time}
                                 imgUrl={meal.imgUrl}
                                 prepTime={meal.prepTime}
                             />
--- noomies-foodies-client/src/components/Planner/Planner.js
+++ noomies-foodies-client/src/components/Planner/Planner.js
@@ -9,62 +9,238 @@ const Planner = () => {
     //todo add async functions to have get request for data. 
     const arrMealsWeek = [
         {
-            weekday: "Monday",
-            date: "4/1/2024",
-            meals: [
-                {
-                    mealTitle: "High-Protein Scrambled Eggs with Cottage Cheese",
-                    mealTime: "Breakfast",
-                    calories: 350,
-                    time: 40,
-                    imgUrl: "./images/planner-icons/sample-recipe.png"
-                },
-                {
-                    mealTitle: "Grilled Chicken Salad with Avocado",
-                    mealTime: "Lunch",
-                    calories: 500,
-                    time: 30,
-                    imgUrl: "./images/planner-icons/sample-recipe.png"
-                },
-                {
-                    mealTitle: "Spaghetti Squash with Turkey Meatballs",
-                    mealTime: "Dinner",
-                    calories: 450,
-                    time: 10,
-                    imgUrl: "./images/planner-icons/sample-recipe.png"
-                }
-            ]
-        },
-        {
             weekday: "Tuesday",
-            date: "4/2/2024",
+            date: "4/23/2024",
             meals: [
                 {
+                    id: 1,
                     mealTitle: "Oatmeal with Berries",
                     mealTime: "Breakfast",
                     calories: 300,
                     time: 20,
+                    prepTime: 10,
                     imgUrl: "./images/planner-icons/sample-recipe.png"
                 },
                 {
+                    id: 2,
                     mealTitle: "Grilled Salmon with Quinoa",
                     mealTime: "Lunch",
                     calories: 550,
                     time: 25,
+                    prepTime: 15,
                     imgUrl: "./images/planner-icons/sample-recipe.png"
                 },
                 {
+                    id: 3,
                     mealTitle: "Vegetable Stir-Fry with Tofu",
                     mealTime: "Dinner",
                     calories: 400,
                     time: 200,
+                    prepTime: 30,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        },
+        {
+            weekday: "Wednesday",
+            date: "4/24/2024",
+            meals: [
+                {
+                    id: 4,
+                    mealTitle: "Scrambled Eggs with Avocado",
+                    mealTime: "Breakfast",
+                    calories: 400,
+                    time: 15,
+                    prepTime: 10,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 5,
+                    mealTitle: "Turkey Sandwich with Salad",
+                    mealTime: "Lunch",
+                    calories: 450,
+                    time: 20,
+                    prepTime: 10,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 6,
+                    mealTitle: "Spaghetti with Marinara Sauce",
+                    mealTime: "Dinner",
+                    calories: 600,
+                    time: 30,
+                    prepTime: 20,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        },
+        {
+            weekday: "Thursday",
+            date: "4/25/2024",
+            meals: [
+                {
+                    id: 7,
+                    mealTitle: "Greek Yogurt Parfait",
+                    mealTime: "Breakfast",
+                    calories: 250,
+                    time: 10,
+                    prepTime: 5,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 8,
+                    mealTitle: "Chicken Caesar Salad",
+                    mealTime: "Lunch",
+                    calories: 400,
+                    time: 20,
+                    prepTime: 15,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 9,
+                    mealTitle: "Beef Tacos",
+                    mealTime: "Dinner",
+                    calories: 500,
+                    time: 30,
+                    prepTime: 20,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        },
+        {
+            weekday: "Friday",
+            date: "4/26/2024",
+            meals: [
+                {
+                    id: 10,
+                    mealTitle: "Banana Pancakes",
+                    mealTime: "Breakfast",
+                    calories: 350,
+                    time: 20,
+                    prepTime: 10,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 11,
+                    mealTitle: "Vegetarian Sushi Rolls",
+                    mealTime: "Lunch",
+                    calories: 300,
+                    time: 25,
+                    prepTime: 20,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 12,
+                    mealTitle: "Grilled Chicken with Roasted Vegetables",
+                    mealTime: "Dinner",
+                    calories: 450,
+                    time: 35,
+                    prepTime: 25,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        },
+        {
+            weekday: "Saturday",
+            date: "4/27/2024",
+            meals: [
+                {
+                    id: 13,
+                    mealTitle: "Avocado Toast with Poached Eggs",
+                    mealTime: "Breakfast",
+                    calories: 400,
+                    time: 20,
+                    prepTime: 15,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 14,
+                    mealTitle: "Quinoa Salad with Chickpeas",
+                    mealTime: "Lunch",
+                    calories: 350,
+                    time: 15,
+                    prepTime: 10,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 15,
+                    mealTitle: "Grilled Steak with Baked Potatoes",
+                    mealTime: "Dinner",
+                    calories: 600,
+                    time: 40,
+                    prepTime: 25,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        },
+        {
+            weekday: "Sunday",
+            date: "4/28/2024",
+            meals: [
+                {
+                    id: 16,
+                    mealTitle: "Blueberry Pancakes",
+                    mealTime: "Breakfast",
+                    calories: 450,
+                    time: 25,
+                    prepTime: 15,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 17,
+                    mealTitle: "Caprese Sandwich with Tomato Soup",
+                    mealTime: "Lunch",
+                    calories: 500,
+                    time: 30,
+                    prepTime: 20,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 18,
+                    mealTitle: "Baked Salmon with Asparagus",
+                    mealTime: "Dinner",
+                    calories: 550,
+                    time: 35,
+                    prepTime: 20,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"   
+                }
+            ]
+        },
+        {
+            weekday: "Monday",
+            date: "4/29/2024",
+            meals: [
+                {
+                    id: 19,
+                    mealTitle: "Fruit Smoothie Bowl",
+                    mealTime: "Breakfast",
+                    calories: 300,
+                    time: 15,
+                    prepTime: 10,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 20,
+                    mealTitle: "Mediterranean Salad with Hummus",
+                    mealTime: "Lunch",
+                    calories: 400,
+                    time: 20,
+                    prepTime: 15,
+                    imgUrl: "./images/planner-icons/sample-recipe.png"
+                },
+                {
+                    id: 21,
+                    mealTitle: "Teriyaki Chicken with Rice",
+                    mealTime: "Dinner",
+                    calories: 500,
+                    time: 30,
+                    prepTime: 20,
                     imgUrl: "./images/planner-icons/sample-recipe.png"   
                 }
             ]
         }
-        
-       
     ];
+    
 
     const meals =  [
         {
@@ -72,6 +248,7 @@ const Planner = () => {
             mealTime: "Breakfast",
             calories: 300,
             time: 20,
+            prepTime: 30,
             imgUrl: "./images/planner-icons/sample-recipe.png"
         },
         {
@@ -79,6 +256,7 @@ const Planner = () => {
             mealTime: "Lunch",
             calories: 550,
             time: 25,
+            prepTime: 30,
             imgUrl: "./images/planner-icons/sample-recipe.png"
         },
         {
@@ -86,6 +264,7 @@ const Planner = () => {
             mealTime: "Dinner",
             calories: 400,
             time: 200,
+            prepTime: 15,
             imgUrl: "./images/planner-icons/sample-recipe.png"   
         }
     ]
