0faa1e12532a51f01afa53fd3eaafcf9fc22dc65
Donte Ladatto <donte279@gmail.com>
2024-04-22 22:45:01 -0500

    add seed file

--- rails-api/db/seeds.rb
+++ rails-api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding Test User"
+test = User.new
+test.email = 'test@example.com'
+test.password = "password"
+test.save!
+
+test.recipes.create([
+  { name: "food", image: "", cook_time: 4, prep_time: 4, yields: 4, description: "Just do it", category: "Breakfast"},
+  { name: "lunch sandwich", image: "", cook_time: 5, prep_time: 3, yields: 8, description: "figure it out", category: "Lunch"},
+  { name: "meat", image: "", cook_time: 50, prep_time: 65, yields: 3, description: "cook it", category: "Dinner"},
+  { name: "cream", image: "", cook_time: 1, prep_time: 1, yields: 1, description: "yum", category: "Dessert"},
+  { name: "help", image: "", cook_time: 2, prep_time: 4, yields: 6, description: "whoops", category: "Snack"}
+])
